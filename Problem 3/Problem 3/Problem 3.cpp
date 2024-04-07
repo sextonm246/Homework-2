c791b51d37bfd8709ba1d7de5afc860248a913a7
sextonm246 <mrs6497@psu.edu>
2024-04-07 13:52:24 -0400

    Redo Question 2

--- Problem 3/Problem 3/Problem 3.cpp
+++ Problem 3/Problem 3/Problem 3.cpp
@@ -2,3 +2,106 @@
 // Problem 3
 
 
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class AirTrafficController {
+private:
+    bool asleep; // Whether the ATC is asleep or not
+    int trafficPattern; // Number of aircraft in the traffic pattern
+
+public:
+    AirTrafficController() : asleep(true), trafficPattern(0) {}
+
+    // Pilot initiates communication with ATC
+    void initiateCommunication(int AircraftNumber) {
+        if (asleep) {
+            asleep = false;
+        }
+        cout << "Aircraft #" << AircraftNumber << " is requesting to land" << endl;
+    }
+
+    // ATC starts communication with a pilot
+    void startCommunication() {
+        if (!asleep) {
+            cout << "ATC is talking to a pilot." << endl;
+        }
+    }
+
+    // ATC finishes communication with a pilot
+    void finishCommunication(int AircraftNumber) {
+        cout << "ATC finished talking to Pilot " << AircraftNumber << "." << endl;
+        asleep = true;
+    }
+
+    // Check if the traffic pattern is full
+    bool isTrafficPatternFull() {
+        return trafficPattern >= 3;
+    }
+
+    // Add an aircraft to the traffic pattern
+    void addAircraft(int AircraftNumber) {
+        if (trafficPattern < 3) {
+            ++trafficPattern;
+            cout << "Aircraft with Pilot " << AircraftNumber << " entered the traffic pattern." << endl;
+        }
+    }
+
+    // Remove an aircraft from the traffic pattern
+    void removeAircraft(int AircraftNumber) {
+        if (trafficPattern > 0) {
+            --trafficPattern;
+            cout << "Aircraft with Pilot " << AircraftNumber << " left the traffic pattern." << endl;
+        }
+    }
+};
+
+int main() {
+    AirTrafficController atc;
+    queue<int> arrivalTimes; // Arrival times of aircraft
+
+    // Generate arrival times and pilot numbers for 10 aircraft
+    for (int i = 1; i <= 10; ++i) {
+        arrivalTimes.push(i);
+    }
+
+    // Simulation loop
+    while (!arrivalTimes.empty()) {
+        int currentTime = arrivalTimes.front();
+        arrivalTimes.pop();
+        cout << "Current time: " << currentTime << "s" << endl;
+
+        // Check if there is an incoming aircraft
+        if (!arrivalTimes.empty()) {
+            int nextArrival = arrivalTimes.front();
+            // If the next arrival is now or after the current time
+            if (nextArrival <= currentTime) {
+                int AircraftNumber = arrivalTimes.size() + 1; // Assign pilot number
+                // Pilot initiates communication
+                atc.initiateCommunication(AircraftNumber);
+
+                // If ATC is talking to another pilot, try again later
+                if (atc.isTrafficPatternFull()) {
+                    cout << "Traffic pattern is full. Aircraft with Pilot " << AircraftNumber << " needs to divert to other airports." << endl;
+                }
+                else {
+                    // Otherwise, add the aircraft to the traffic pattern
+                    atc.addAircraft(AircraftNumber);
+                }
+            }
+        }
+
+        // If the ATC finishes talking to a pilot, remove the aircraft from the traffic pattern
+        atc.finishCommunication(arrivalTimes.size() + 1);
+        atc.removeAircraft(arrivalTimes.size() + 1);
+
+        // Check if all aircraft have arrived
+        if (arrivalTimes.empty()) {
+            cout << "All aircraft have arrived." << endl;
+        }
+    }
+
+    return 0;
+}
--- Question 2/Question 2/Question 2.cpp
+++ Question 2/Question 2/Question 2.cpp
@@ -1,37 +1,74 @@
 // Michael Sexton
 // Homework 2
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
 
 using namespace std;
 
+// Declare mutex object outside of the function
+mutex mtx;
+
+// Function to simulate the task performed by a robot
+void performTask(int robotNumber, vector<bool>& tools, int& time) {
+    {
+        unique_lock<mutex> lock(mtx);
+        cout << "Robot " << robotNumber << " is collecting data." << endl;
+    }
+
+    int leftToolIndex = (robotNumber + 3) % 5; // Index of left tool
+    int rightToolIndex = (robotNumber + 2) % 5; // Index of right tool
+
+    // Acquire tools
+    {
+        unique_lock<mutex> lock(mtx);
+        while (!tools[leftToolIndex] || !tools[rightToolIndex]) {
+            // Wait until both tools are available
+        }
+        tools[leftToolIndex] = false;
+        tools[rightToolIndex] = false;
+        cout << "Robot " << robotNumber << " acquired tools and started performing a task." << endl;
+        if (robotNumber == 1 || robotNumber == 2 || robotNumber == 5) {
+            time += 1; // Increase time by 1 for picking up tools
+        }
+    }
+
+    // Return tools
+    {
+        unique_lock<mutex> lock(mtx);
+        tools[leftToolIndex] = true;
+        tools[rightToolIndex] = true;
+        cout << "Robot " << robotNumber << " finished the task and returning tools." << endl;
+        if (robotNumber == 1 || robotNumber == 2 || robotNumber == 5) {
+            time += 5; // Increase time by 5 for completing the task
+        }
+    }
+}
+
 int main() {
-	int time = 0;
-	string robotstatus1; string robotstatus2;
-	int robotnumber1 = 1; int robotnumber2=4;
+    vector<bool> tools(5, true); // Initially, all tools are available
+    int time = 0; // Initialize time counter 
 
-	while (robotnumber1 < 3) {
-		cout << "Robot " << robotnumber1 << " acquired tools and started performing a task." << endl;
-		cout << "Robot " << robotnumber2 << " acquired tools and started performing a task." << endl;
-		time = time + 1; 
+    // Robots 1 and 3 work simultaneously
+    thread t1(performTask, 1, ref(tools), ref(time));
+    // Robots 2 and 4 work simultaneously
+    thread t2(performTask, 2, ref(tools), ref(time));
+    // Robot 3 works (with 1)
+    thread t3(performTask, 3, ref(tools), ref(time));
+    // Robot 4 works (with 2)
+    thread t4(performTask, 4, ref(tools), ref(time));
+    // Robot 5 works
+    thread t5(performTask, 5, ref(tools), ref(time));
 
-		cout << "Robot " << robotnumber1 << " has finished the task and is returning the tools." << endl;
-		cout << "Robot " << robotnumber2 << " has finished the task and is returning the tools." << endl;
-		time = time + 5;
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
 
-		robotnumber1++; robotnumber2++;
-	}
+    cout << "Total time: " << time << endl;
 
-	if (robotnumber1 == 3) {
-		cout << "Robot " << robotnumber1 << " acquired tools and started performing a task." << endl;
-		time = time + 1;
-
-		cout << "Robot " << robotnumber1 << " has finished the task and is returning the tools." << endl;
-		time = time + 5;
-	}
-
-	cout << "Duration: " << time << " seconds." << endl;
-
-	return 0;
+    return 0;
 }
